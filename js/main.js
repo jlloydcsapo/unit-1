ee9bdbcd080bca9e94a630bc0fcdbfc02a39d0c3
jlloydcsapo <lloydcsapo@outlook.com>
2023-06-08 18:43:55 -0400

    Activity 4

--- js/main.js
+++ js/main.js
@@ -1,84 +1,146 @@
-var mydiv = document.getElementById("mydiv");
-mydiv.addEventListener("click", function(){
-  alert("Hello World! You're doing it");  
-});
+var cityPop = [
+	{ 
+		city: 'Madison',
+		population: 233209
+	},
+	{
+		city: 'Milwaukee',
+		population: 594833
+	},
+	{
+		city: 'Green Bay',
+		population: 104057
+	},
+	{
+		city: 'Superior',
+		population: 27244
+	}
+];
 
-var classdivs = document.getElementsByClassName("classdiv");
+//create table element
+var table = document.createElement("table");
 
-var writeFunction = function() {
-    document.write("How about this?");
-};
-
-for (var i = 0; i < classdivs.length; i++) {
-    classdivs[i].addEventListener('dblclick', writeFunction);
-};
-
-//initialize function called when the script loads
 function initialize(){
-    cities();
-    myFunc();
+    buildTable();
+    addColumns();
+    addEvents();
+	debugAjax();
 };
 
-//function to create a table with cities and their populations
-function cities(){
-    //define two arrays for cities and population
-    var cities = [
-        'Madison',
-        'Milwaukee',
-        'Green Bay',
-        'Superior'
-    ];
-    var population = [
-        233209,
-        594833,
-        104057,
-        27244
-    ];
-
-    //create the table element
-    var table = document.createElement("table");
+//build table from existing data
+function buildTable(){
 
     //create a header row
     var headerRow = document.createElement("tr");
 
-    //add the "City" column
-    var cityHeader = document.createElement("th");
-    cityHeader.innerHTML = "City";
-    headerRow.appendChild(cityHeader);
-
-    //add the "Population" column
-    var popHeader = document.createElement("th");
-    popHeader.innerHTML = "Population";
-    headerRow.appendChild(popHeader);
-
-    //add the row to the table
+    //add the "City" and "Population" columns to the header row
+    headerRow.insertAdjacentHTML("beforeend","<th>City</th><th>Population</th>")
     table.appendChild(headerRow);
 
-    //loop to add a new row for each city
-    for (var i = 0; i < cities.length; i++){
+    //loop through cityPop to populate table data
+    for (var i = 0; i < cityPop.length; i++){
         var tr = document.createElement("tr");
 
         var city = document.createElement("td");
-        city.innerHTML = cities[i];
+        city.innerHTML = cityPop[i].city; 
         tr.appendChild(city);
 
         var pop = document.createElement("td");
-        pop.innerHTML = population[i];
+        pop.innerHTML = cityPop[i].population; 
         tr.appendChild(pop);
 
-        table.appendChild(tr);
+    table.appendChild(tr);
     };
+}
 
-    //add the table to the div in index.html
-    var myDiv =  document.getElementById("tableDiv");
-    myDiv.appendChild(table);
+//Add city Size column to table
+function addColumns(){
+    
+    //select all existing rows of the previously generated table
+    //for each, add appropriate column data
+    document.querySelectorAll("tr").forEach(function(row, i){
+
+    	if (i == 0){
+    		row.insertAdjacentHTML('beforeend', '<th>City Size</th>');
+    	} else {
+
+    		var citySize;
+            
+            //conditional to determine city siz
+    		if (cityPop[i-1].population < 100000){
+    			citySize = 'Small';
+
+    		} else if (cityPop[i-1].population < 500000){
+    			citySize = 'Medium';
+
+    		} else {
+    			citySize = 'Large';  
+    		};
+
+			row.insertAdjacentHTML('beforeend', '<td>' + citySize + '</td>');
+    	};
+
+    });
 };
 
-//call the initialize function when the window has loaded
-
-function myFunc(){
-    var myAutoDiv = document.getElementById("myAutoDiv");
-    myAutoDiv.innerHTML = "Let's script some html!"
+function clickme(){
+    alert('Hey, you clicked me!');
 };
 
-window.onload = initialize();
+//add events to table
+function addEvents(){
+    //add click event using predefined function 'clickme' above
+    document.querySelector("table").addEventListener("click", clickme);
+
+    //add mouseover event using anonymous function
+	document.querySelector("table").addEventListener("mouseover", function() {
+		
+		    var color = "rgb(";
+
+		    for (var i=0; i<3; i++){
+
+			    var random = Math.round(Math.random() * 255);
+
+			    color += random;
+
+			    if (i<2){
+		    		color += ",";
+			
+			    } else {
+		    		color += ")";
+	    	};
+            console.log(color)
+		    
+            document.querySelector("table").style.color = color;
+	    };
+    });
+}
+
+//define debugCallback function = expects response.json 
+function debugCallback(response){
+
+	//assign response  to m
+	var myData = response;
+
+	//convert json to string and appent to mydiv element 
+	document.querySelector("#mydiv").insertAdjacentHTML('beforeend', 'GeoJSON data: ' + JSON.stringify(myData))
+};
+
+//define ajax call
+function debugAjax(){
+	
+	//fetches file map.geojson
+	fetch('data/map.geojson')
+		//converts response to usable json format
+		.then(function(response){
+			return response.json();
+		})
+		//callback function 
+		.then(debugCallback)
+};
+
+//add table to div with id = mydiv in index.html
+document.querySelector("#mydiv").appendChild(table);
+
+//ensure scripts are not run until html content is fully downloaded
+document.addEventListener('DOMContentLoaded',initialize)
